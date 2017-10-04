3036efa4f914363dfd1f4d035bac0924e461116a
Linus Unnebäck <linus@folkdatorn.se>
2017-10-04 15:46:43 +0100

    🐛 Give each separator a unique key

--- index.js
+++ index.js
@@ -2,11 +2,19 @@
 
 const React = require('react')
 
+function createSeparator (separator, idx) {
+  if (typeof separator === 'string') {
+    return separator
+  }
+
+  return React.cloneElement(separator, { key: `separator-${idx}` })
+}
+
 module.exports = function WithSeparator (props) {
   const children = React.Children.toArray(props.children)
 
   for (let i = 1; i < children.length; i += 2) {
-    children.splice(i, 0, props.separator)
+    children.splice(i, 0, createSeparator(props.separator, (i - 1) / 2))
   }
 
   return children
